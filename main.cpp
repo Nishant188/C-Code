e2bd02ddfc60b7a1136890f4c9394a84923ee056
Nishant <63123505+Nishant188@users.noreply.github.com>
2021-06-11 01:51:11 +0000

    Insert a value at a particular index position in the array

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Append a value in last position of the array
+// Insert a value at a particular index position in the array.
 #include <iostream>
 struct Array{
  int A[20];
@@ -15,9 +15,21 @@ void Append(struct Array *arr,int x)
   if(arr->length<arr->Size)
     arr->A[arr->length++] = x; 
 }
+void Insert(struct Array *arr,int index,int x){
+  if(index >= 0 && index <= arr->length)
+  {
+    for(int i =arr->length; i>index; i--)
+    {
+      arr->A[i] = arr->A[i-1]; 
+    }
+    arr->A[index] = x;
+    arr->length++;
+  }
+}
 int main() {
   struct Array arr = {{1,2,3,4,5},20,5};
-  Append(&arr,10);
+  // Append(&arr,10);
+  Insert(&arr,4,15);
   display(arr);
 return  0;
 }
