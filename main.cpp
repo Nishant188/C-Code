3051cf42a904c9702ff73b11f0c508616b573226
Nishant <63123505+Nishant188@users.noreply.github.com>
2021-06-11 01:39:47 +0000

    Append a value in last position of the array.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Now I create Fixed Size Array
+// Append a value in last position of the array
 #include <iostream>
 struct Array{
  int A[20];
@@ -6,12 +6,18 @@ struct Array{
  int length; 
 }; 
 void display(struct Array arr){
-  printf("Elements are :/");
+  printf("After Appending Elements are :/");
   for(int i =0 ;i<arr.length;i++)
   printf("\n%d",arr.A[i]);
 }
+void Append(struct Array *arr,int x)
+{
+  if(arr->length<arr->Size)
+    arr->A[arr->length++] = x; 
+}
 int main() {
   struct Array arr = {{1,2,3,4,5},20,5};
+  Append(&arr,10);
   display(arr);
 return  0;
 }
