bad33f45af9c488e09d252a5d374de8eba9f40d1
Nishant <63123505+Nishant188@users.noreply.github.com>
2021-06-11 02:26:11 +0000

    Deleting a value at a particular index position in the array.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Insert a value at a particular index position in the array.
+// Deleting a value at a particular index position in the array.
 #include <iostream>
 struct Array{
  int A[20];
@@ -6,7 +6,7 @@ struct Array{
  int length; 
 }; 
 void display(struct Array arr){
-  printf("After Appending Elements are :/");
+  printf("\nAfter Deleting Elements are :/");
   for(int i =0 ;i<arr.length;i++)
   printf("\n%d",arr.A[i]);
 }
@@ -26,10 +26,25 @@ void Insert(struct Array *arr,int index,int x){
     arr->length++;
   }
 }
+int Delete(struct Array *arr,int index){
+  int n=0;
+  if(index>=0 && index <= arr->length){
+    n = arr->A[index];
+    for(int i = index; i<arr->length-1; i++)
+    {
+      arr->A[i] = arr ->A[i+1];
+    }
+    arr->length--;
+    return n;
+  }
+  return 0;
+}
 int main() {
-  struct Array arr = {{1,2,3,4,5},20,5};
+  struct Array arr1 = {{1,2,3,4,5},20,5};
   // Append(&arr,10);
-  Insert(&arr,4,15);
-  display(arr);
+  // Insert(&arr,4,15);
+  // Delete(&arr1,3);
+  printf("\n%d",Delete(&arr1,0));
+  display(arr1);
 return  0;
 }
