981ea8b575da999ee991e5a89c095f976923cf03
Nishant <63123505+Nishant188@users.noreply.github.com>
2021-06-13 20:10:22 +0000

    improvement In Array

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Linear Searching Array!!
+// Linear Searching Array Improvement!!
 #include <iostream>
 struct Array{
  int A[10];
@@ -9,17 +9,27 @@ void display(struct Array arr1){
   for(int i =0 ;i<arr1.length;i++)
   printf("\n%d",arr1.A[i]);
 }
-int LinearSearch(struct Array arr1,int key){
-  for(int i=0;i<arr1.length;i++)
+void swap(int *x,int *y){
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+int LinearSearch(struct Array *arr1,int key){
+  for(int i=0;i<arr1->length;i++)
   {
-    if(key==arr1.A[i])
-      return i;
+    if(key==arr1->A[i])
+    {
+      // swap(&arr1->A[i], &arr1->A[i-1]); //Transpostion
+      swap(&arr1->A[i], &arr1->A[0]);//Move to front/Head
+      return i-1;
+    }
   }
   return -1;
 }
 int main() {
   struct Array arr1 = {{12,34,45,23,76,89,74,78},10,8};
-  printf("Found At INDEX : %d",LinearSearch(arr1,2));
+  printf("Found At INDEX : %d",LinearSearch(&arr1,78));
   display(arr1);
 return  0;
 }
